97a9748f9acedfa96b4fe64e95d582f98ddf10b9
Brian Michell <brianm314@comcast.net>
2021-02-20 13:22:07 -0600

    Fixed missing import

--- MarketStreet.java
+++ MarketStreet.java
@@ -1,6 +1,7 @@
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
 
 import java.io.IOException;
 import java.util.ArrayList;
