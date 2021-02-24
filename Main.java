4cf679a52c1cdfb047eccb14d812b93c42aa9af0
Brian Michell <brianm314@comcast.net>
2021-02-24 16:46:38 -0600

    Universal test cases

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Target t_test = new Target("https://www.target.com/p/make-a-size-paper-towels-2-big-rolls-smartly-8482/-/A-75557222");
+		WalmartCrawler w_test = new WalmartCrawler("paper towles");
+		MarketStreet m_test = new MarketStreet("paper_towles");
+	}
+
+}
