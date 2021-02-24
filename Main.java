6dada7aba1d55db7ad3cd663aa5ab3acc45655db
Brian Michell <brianm314@comcast.net>
2021-02-24 16:09:31 -0600

    Altered test cases

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		WalmartCrawler c = new WalmartCrawler("bananas");
-		System.out.println("--------------------");
-//		c = new WalmartCrawler("");
-		System.out.println("--------------------");
-//		c = new WalmartCrawler("rug");
-		System.out.println("--------------------");
-		c = new WalmartCrawler("asd;flkj");
+		MarketStreet m = new MarketStreet("chocolate");
+		
+//		WalmartCrawler c = new WalmartCrawler("bananas");
+//		System.out.println("--------------------");
+////		c = new WalmartCrawler("");
+//		System.out.println("--------------------");
+////		c = new WalmartCrawler("rug");
+//		System.out.println("--------------------");
+//		c = new WalmartCrawler("asd;flkj");
 	}
 	
 	public static void runTests() {
