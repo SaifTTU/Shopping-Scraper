e53e6c46ce36c6bf10aa45a6df1e836256fc175c
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2021-04-22 00:47:43 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		WalmartCrawler c = new WalmartCrawler("bananas");
-		System.out.println("--------------------");
-//		c = new WalmartCrawler("");
-		System.out.println("--------------------");
-//		c = new WalmartCrawler("rug");
-		System.out.println("--------------------");
-		c = new WalmartCrawler("asd;flkj");
+		Target t_test = new Target("https://www.target.com/p/make-a-size-paper-towels-2-big-rolls-smartly-8482/-/A-75557222");
+		WalmartCrawler w_test = new WalmartCrawler("paper towels");
+		MarketStreet m_test = new MarketStreet("paper towels");
 	}
 }
